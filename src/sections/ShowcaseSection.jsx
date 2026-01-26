7e36ac35bca325eb756edbeab505b0b63d80ab84
Muhammad Ahmad Faseeh <muhammadahmadfaseeh5@gmail.com>
2026-01-27 00:23:02 +0500

    Update ShowcaseSection.jsx

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -74,7 +74,8 @@ const AppShowcase = () => {
               <div className="image-wrapper bg-[#FFE7EB]">
                 <img src="/images/project3.png" alt="YC Directory App" />
               </div>
-              <h2> Building an Anime website where user will watch anime</h2>
+              <a href="https://anime-vault-beryl-xi.vercel.app/"><h2> Building an Anime website where user will watch anime</h2></a>
+              
             </div>
           </div>
         </div>
